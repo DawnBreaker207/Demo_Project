0abd032f6f89762732f6aa25ae067a3f9260868f
DawnBreaker <tunganhngo207@gmail.com>
2025-04-29 15:37:34 +0700

    feat: update schema

--- server/src/main/resources/schema.sql
+++ server/src/main/resources/schema.sql
@@ -2,47 +2,63 @@ DROP SCHEMA IF EXISTS logistics;
 CREATE SCHEMA logistics;
 USE logistics;
 
-CREATE TABLE IF NOT EXISTS `product` (
+CREATE TABLE IF NOT EXISTS product (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	name VARCHAR(255),
-	price INT
-	
+	name VARCHAR(255) NOT NULL,
+	sku VARCHAR(255) NOT NULL,
+	price INT NOT NULL,
+	is_delete BOOLEAN DEFAULT 0	
 --	CONSTRAINT pk_product_id KEY(id)/
 );
 
-CREATE TABLE IF NOT EXISTS `customer` (
+CREATE TABLE IF NOT EXISTS customer (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	name VARCHAR(255),
+	name VARCHAR(255) NOT NULL,
 	phone VARCHAR(255),
 	address VARCHAR(255)
 
 --	CONSTRAINT pk_customer_id KEY(id)
 );
 
-CREATE TABLE IF NOT EXISTS `order` (
+CREATE TABLE IF NOT EXISTS shipment (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	customer_id INT,
-	total INT,
-	status ENUM('PENDING', 'SUCCESS', 'FAILED'),
-	payment_status ENUM('PENDING', 'SUCCESS', 'FAILED'),
-	created_at DATE,
+	method ENUM('COD','BANK') NOT NULL,
+	status ENUM('SUCCESS', 'PENDING', 'FAILED') DEFAULT 'PENDING',
+	amount INT NOT NULL,
+	paid_at DATE
 	
-	FOREIGN KEY (customer_id) REFERENCES customer (id)
+--	CONSTRAINT pk_shipment_id KEY (id),
+--	CONSTRAINT pk_shipment_order_id KEY (order_id) REFERENCES order(order_id) 
+);
+
+
+CREATE TABLE IF NOT EXISTS orders (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	customer_id INT NOT NULL,
+	shipment_id INT,
+	total INT NOT NULL,
+	status ENUM('SUCCESS', 'PENDING', 'FAILED') DEFAULT 'PENDING',
+	payment_status ENUM('SUCCESS','PENDING', 'FAILED') DEFAULT 'PENDING',
+	created_at DATE DEFAULT (CURRENT_DATE),
+	is_delete BOOLEAN DEFAULT 0,
+	
+	FOREIGN KEY (customer_id) REFERENCES customer(id),
+	FOREIGN KEY (shipment_id) REFERENCES shipment(id)
 	
 --	CONSTRAINT pk_order_id KEY (id),
 --	CONSTRAINT fk_order_customer_id FOREIGN KEY (customer_id),
 --	REFERENCES customer (customer_id)
 );
 
-CREATE TABLE IF NOT EXISTS `order_item` (
+CREATE TABLE IF NOT EXISTS order_item (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	order_id INT,
-	product_id INT,
-	quantity INT,
-	price INT,
+	order_id INT NOT NULL,
+	product_id INT NOT NULL,
+	quantity INT NOT NULL,
+	price INT NOT NULL,
 	
-	FOREIGN KEY (order_id) REFERENCES `order` (id),
-	FOREIGN KEY (product_id) REFERENCES product (id)
+	FOREIGN KEY (order_id) REFERENCES orders(id ),
+	FOREIGN KEY (product_id) REFERENCES product(id)
 	
 --	CONSTRAINT pk_order_item_id KEY (id),
 --	CONSTRAINT fk_order KEY (order_id) REFERENCES order(order_id),
@@ -50,15 +66,3 @@ CREATE TABLE IF NOT EXISTS `order_item` (
 	
 );
 
-CREATE TABLE IF NOT EXISTS `shipment` (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	order_id INT,
-	method ENUM('COD','BANK'),
-	amount INT,
-	paid_at DATE,
-	
-	FOREIGN KEY (order_id) REFERENCES `order` (id)
-	
---	CONSTRAINT pk_shipment_id KEY (id),
---	CONSTRAINT pk_shipment_order_id KEY (order_id) REFERENCES order(order_id) 
-);
