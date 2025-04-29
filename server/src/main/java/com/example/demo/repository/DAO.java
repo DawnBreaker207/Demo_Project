ea416fbbe7ecdc4df4ec0ab8d6129d37eb677cc8
DawnBreaker <tunganhngo207@gmail.com>
2025-04-29 15:39:08 +0700

    feat: update DAO

--- server/src/main/java/com/example/demo/repository/DAO.java
+++ server/src/main/java/com/example/demo/repository/DAO.java
@@ -4,13 +4,13 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface DAO<T> {
-    public List<T> getAll() throws SQLException;
+    List<T> getAll() throws SQLException;
 
-    public T getOne(long id) throws SQLException;
+    T getOne(long id) throws SQLException;
 
-    public T create(T t) throws SQLException;
+    T create(T t) throws SQLException;
 
-    public T update(T t) throws SQLException;
+    T update(T t) throws SQLException;
 
-    public void delete(int id) throws SQLException;
+    void delete(long id) throws SQLException;
 }
